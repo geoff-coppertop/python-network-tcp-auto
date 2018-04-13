7c26f46aeedf176e04aa9ddd9ecf3d7904109042
Geoffrey Thomas <geoffrey.thomas@garmin.com>
2018-04-13 14:55:23 -0600

    Initial commit of Client and Server modules
    Adds the Client and Server modules
     - Able to do auto discovery of each other
     - Communicate using simple byte protocol where 4 bytes of size are
       transmitted first
    
    Adds a setup script to install the Client and Server modules
    
    Adds a manual test harness that is able to run the client and server
    
    Change-Id: I74fb18f1cce61cdf37827fee507750dc25768469

---
+++ setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#-------------------------------------------------------------------------------
+# setup.py
+#
+# G. Thomas
+# 2018
+#-------------------------------------------------------------------------------
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+from glob import glob
+from os.path import basename
+from os.path import dirname
+from os.path import join
+from os.path import relpath
+from os.path import splitext
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='auto-tcp-network',
+    description='Auto configuring TCP network for simple data transmission',
+    long_description=long_description,
+    url='https://github.com/geoff-coppertop/auto-tcp-network',
+    author='Geoffrey Thomas',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: System :: Networking',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='tcp zeroconf network',  # Optional
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    # py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,
+    use_scm_version=True,
+    setup_requires=[
+        'setuptools_scm'
+    ],
+    install_requires=[
+        'aiozeroconf',
+        'transitions',
+    ],
+    project_urls={
+        'Bug Reports': 'https://github.com/geoff-coppertop/auto-tcp-network/issues',
+        'Source': 'https://github.com/geoff-coppertop/auto-tcp-network/',
+    },
+)
